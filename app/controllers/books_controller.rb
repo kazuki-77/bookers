54d5308823ff3a96c65d3cdbb0340566708298fa
kazuki-77 <93814427+kazuki-77@users.noreply.github.com>
2021-12-05 14:53:04 +0000

    [update] 3-8　showページ、editページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -19,6 +20,20 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.delete
+    redirect_to books_path
   end
 
   private
